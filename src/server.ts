5905aeba27e020d6fb3810b62130aec43950459e
Umme-hanii <ummehanimohammed9@gmail.com>
2023-12-05 12:19:16 +0530

    Install cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express'
+import cors from 'cors'
 import { router } from './api/routes/index'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use('/api/', router)
 
