59849d279a505aedb5989f27bc1c671626ec6a72
Umme-hanii <ummehanimohammed9@gmail.com>
2023-12-06 02:42:29 +0530

    Not found Route

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import express from 'express'
 import cors from 'cors'
 import { router } from './api/routes/index'
+import { notFound } from './api/middleware/not-found'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use('/api/', router)
+
+app.use(notFound)
 
 app.listen(8080, () => {
   console.log('Server is listening on port 8080')
